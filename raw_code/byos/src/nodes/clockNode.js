83c4b8b5b46884db3b34822092b0dbd1db5ba8eb
Kuba Karpierz <mr.karpierz@gmail.com>
2024-12-01 19:34:53 -0500

    new build

--- raw_code/byos/src/nodes/clockNode.js
+++ raw_code/byos/src/nodes/clockNode.js
@@ -14,9 +14,10 @@ class ClockNode {
 
   schedule() {
     const currentTime = this.context.currentTime;
+    const minBPM = Math.max(1, this.bpm);
     const nextNoteTime =
-      currentTime + 60 / this.bpm - (currentTime % (60 / this.bpm));
-    const nextNextNoteTime = nextNoteTime + 60 / this.bpm;
+      currentTime + 60 / minBPM - (currentTime % (60 / minBPM));
+    const nextNextNoteTime = nextNoteTime + 60 / minBPM;
     for (let i = 0; i < this.connections.length; i++) {
       const connection = this.connections[i];
       connection.applyEnvelope(nextNoteTime);
@@ -24,7 +25,7 @@ class ClockNode {
     }
     setTimeout(() => {
       this.schedule();
-    }, 30 / this.bpm);
+    }, 30 / Math.max(60, minBPM));
   }
 
   static dataFromString(str) {

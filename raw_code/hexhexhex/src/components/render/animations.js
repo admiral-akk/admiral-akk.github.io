9224d2911b0be42c0ea86a98dbf3246d1291c9bd
Kuba Karpierz <mr.karpierz@gmail.com>
2024-12-29 18:28:09 -0500

    Removed particle system

--- raw_code/hexhexhex/src/components/render/animations.js
+++
@@ -1,8 +0,0 @@
-import { Component } from "../../ecs/component";
-import { Queue } from "../../util/queue";
-
-export class Animations extends Component {
-  constructor() {
-    this.animationQueue = new Queue();
-  }
-}

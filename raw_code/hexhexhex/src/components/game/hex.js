259aa7807fd9a110b57aa1b37823418479be5f40
Kuba Karpierz <mr.karpierz@gmail.com>
2025-01-01 15:45:23 -0500

    remove code

--- raw_code/hexhexhex/src/components/game/hex.js
+++ raw_code/hexhexhex/src/components/game/hex.js
@@ -3,10 +3,6 @@ import { Component } from "../../ecs/component";
 const hexMap = new Map();
 
 export class Hex extends Component {
-  static get(coords) {
-    return hexMap.get(`${coords[0]},${coords[1]}`);
-  }
-
   constructor(coords) {
     super();
     this.coords = coords;

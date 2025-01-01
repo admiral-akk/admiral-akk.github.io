5ce9f0538a5031dc8690950a1c2243cf9337d30c
Kuba Karpierz <mr.karpierz@gmail.com>
2025-01-01 16:12:51 -0500

    added options

--- raw_code/hexhexhex/src/systems/render/markSelected.js
+++ raw_code/hexhexhex/src/systems/render/markSelected.js
@@ -1,7 +1,6 @@
 import { System } from "../../ecs/system.js";
 import { Selected } from "../../components/client/selected.js";
 import { Transform } from "../../components/render/transform.js";
-import { Hex } from "../../components/game/hex.js";
 
 class MarkSelected extends System {
   constructor(markerEntity) {
@@ -18,6 +17,10 @@ class MarkSelected extends System {
     if (entity.components.resource) {
       this.markerEntity.components.transform.setPosition([0, 0, 0]);
       this.markerEntity.components.transform.setScale([0.2, 0.2, 0.2]);
+    }
+    if (entity.components.option) {
+      this.markerEntity.components.transform.setPosition([0, 0, 0]);
+      this.markerEntity.components.transform.setScale([0.3, 0.3, 0.3]);
     }
   }
 }

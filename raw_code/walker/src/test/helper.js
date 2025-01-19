a22d0c784e2494d769472a0ab98cb728b2c98e98
Kuba Karpierz <mr.karpierz@gmail.com>
2025-01-19 14:42:47 -0500

    refactor distance

--- raw_code/walker/src/test/helper.js
+++ raw_code/walker/src/test/helper.js
@@ -16,7 +16,7 @@ export const expectedApprox = (v, expected, obj = {}, epsilon = 0.001) => {
 
 export const distance = (v1, v2) => {
   var actualDistance;
-  const type = typeof actual;
+  var type = typeof v1;
 
   if (v1 instanceof Vec3) {
     actualDistance = Vec3.distance(v1, v2);
@@ -51,31 +51,7 @@ export const approxDistance = (
   debugObj = {},
   epsilon = 0.001
 ) => {
-  var actualDistance;
-  var type = typeof actual;
-
-  if (actual instanceof Vec3) {
-    actualDistance = Vec3.distance(actual, expected);
-  } else if (type === "number" || actual instanceof Number) {
-    actualDistance = actual - expected;
-  } else if (actual instanceof Triangle) {
-    var minError = 100000000;
-    for (let i = 0; i < 3; i++) {
-      var totalError = 0;
-      for (let j = 0; j < 3; j++) {
-        const p1 = actual.points[(j + i) % 3];
-        const p2 = expected.points[j];
-        totalError += Vec3.distance(p1, p2);
-      }
-      minError = Math.min(minError, totalError);
-    }
-
-    actualDistance = minError;
-  } else {
-    throw new Error(
-      `Unknown actual type: ${actual.prototype.constructor.toString()}`
-    );
-  }
+  var actualDistance = distance(actual, expected);
 
   assert.isAtMost(
     Math.abs(actualDistance - expectedDistance),

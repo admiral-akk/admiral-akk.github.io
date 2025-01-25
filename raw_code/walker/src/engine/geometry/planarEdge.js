aee27f01ec1a90edb1ff84ce2e1502aa55379bfc
Kuba Karpierz <mr.karpierz@gmail.com>
2025-01-25 16:02:49 -0500

    Added planar edge

--- raw_code/walker/src/engine/geometry/planarEdge.js
+++ raw_code/walker/src/engine/geometry/planarEdge.js
@@ -1,14 +1,46 @@
+import { Vec3 } from "gl-matrix";
 import { Primative } from "./primative";
 
 export class PlanarEdge extends Primative {
   static makePlanarEdge({ plane1, plane2, planes, metadata }) {
     // check if there is an edge between the planes
 
-    // find the bottom
+    const line = plane1.planeIntersection(plane2);
 
-    // find the top
+    if (line === null) {
+      return null;
+    }
 
-    return null;
+    var start = null;
+    var end = null;
+
+    planes.forEach((p) => {
+      const t = line.lineToPlaneT(p);
+      if (t === null) {
+        return;
+      }
+      if (Vec3.dot(p.normal, line.dir) > 0) {
+        // start
+        if (start === null || line.lineToPlaneT(start) < t) {
+          start = p;
+        }
+      } else {
+        // end
+        if (end === null || line.lineToPlaneT(end) > t) {
+          end = p;
+        }
+      }
+    });
+
+    if (
+      start !== null &&
+      end !== null &&
+      line.lineToPlaneT(start) + 0.001 > line.lineToPlaneT(end)
+    ) {
+      return null;
+    }
+
+    return new PlanarEdge(plane1, plane2, start, end, metadata);
   }
 
   constructor(plane1, plane2, start, end, metadata = {}) {

65e24eacd0ff81ff5b0118f5b45835e4abacf339
Kuba Karpierz <mr.karpierz@gmail.com>
2025-02-15 17:36:07 -0500

    changed angle

--- raw_code/wasm/src/script.js
+++ raw_code/wasm/src/script.js
@@ -56,21 +56,21 @@ const program = createProgram(gl, vertexShaderSource, fragmentShaderSource);
 const quadProgram = createPostProcessProgram(gl, quadFragmentShaderSource);
 const renderTexture = createPostProcessProgram(gl, renderTextureSource);
 
-const cameraBaseT = new Transform({ pos: [0, -1, 0] });
+const cameraBaseT = new Transform({ pos: [0, 0, 0] });
 
 const horizontalRotT = new Transform({
   parent: cameraBaseT,
-  rot: Quat.create().rotateY(-Math.PI / 4 + 0),
+  rot: Quat.create().rotateY(-Math.PI / 4),
 });
 
 const verticalRotT = new Transform({
   parent: horizontalRotT,
-  rot: Quat.create().rotateX(Math.PI / 4 - 0.4),
+  rot: Quat.create().rotateX(Math.PI / 4),
 });
 
 const zoomT = new Transform({
   parent: verticalRotT,
-  pos: [0, 0, -4],
+  pos: [0, 0, -6],
 });
 
 const cameraBase = new Entity(cameraBaseT);

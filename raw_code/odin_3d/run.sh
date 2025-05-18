ffaeb191de3ff196de026f14136a4e4b71706855
Kuba Karpierz <mr.karpierz@gmail.com>
2025-05-18 12:30:55 -0400

    added clean up to run

--- raw_code/odin_3d/run.sh
+++ raw_code/odin_3d/run.sh
@@ -1,3 +1,5 @@
+rm -rf ./dist/*
+mkdir dist
 ./build.sh
 odin build src/main.odin -file -out:dist/main
 install_name_tool -add_rpath /Users/kuba/Documents/source_code/Odin/vendor/raylib/macos/ dist/main

9b9af3ef3372f9ffe61a6b554e8d2bea5256c290
Kuba Karpierz <mr.karpierz@gmail.com>
2025-09-03 17:52:18 -0400

    added index

---
+++ raw_code/learn_webrtc/server/index.js
@@ -0,0 +1,78 @@
+import express from "express";
+import { WebSocketServer } from "ws";
+import wrtc from "@koush/wrtc";
+import { randomUUID } from "crypto";
+
+const app = express();
+app.use(express.static("public")); // serve browser files
+
+const server = app.listen(3000, () =>
+  console.log("Server running on http://localhost:3000")
+);
+
+// WebSocket server for signaling
+const wss = new WebSocketServer({ server });
+
+// keep alive
+const rooms = {};
+const players = {};
+
+const addPlayer = ({ dc, playerId = randomUUID() }) => {
+  players[playerId] = { dc };
+  return playerId;
+};
+
+const createRoom = ({ name, capacity, hostId }) => {
+  const roomId = randomUUID();
+  rooms[roomId] = {
+    name,
+    capacity,
+    players: [{ playerId: hostId, latest: 0 }],
+    messages: [],
+  };
+  return roomId;
+};
+
+const joinRoom = ({ playerId, roomId }) => {
+  rooms[roomId].players.push({ playerId, latest: 0 });
+};
+
+wss.on("connection", (ws) => {
+  console.log("Browser connected via WS");
+
+  const pc = new wrtc.RTCPeerConnection();
+
+  // Listen for incoming DataChannel from browser
+  pc.ondatachannel = (event) => {
+    const dc = event.channel;
+    dc.onopen = () => console.log("DataChannel open on Node");
+    dc.onmessage = (msg) => console.log("Node received:", msg.data);
+
+    // send a reply
+    dc.send("Hello from Node!");
+    dc.send("Hello from Node2!");
+  };
+
+  // ICE candidates from Node → browser
+  pc.onicecandidate = ({ candidate }) => {
+    if (candidate) ws.send(JSON.stringify({ type: "ice", candidate }));
+  };
+
+  // Handle signaling messages from browser
+  ws.on("message", async (msg) => {
+    const data = JSON.parse(msg.toString());
+
+    if (data.type === "offer") {
+      await pc.setRemoteDescription(data);
+      const answer = await pc.createAnswer();
+      await pc.setLocalDescription(answer);
+      ws.send(JSON.stringify(pc.localDescription));
+    } else if (data.type === "ice") {
+      try {
+        await pc.addIceCandidate(data.candidate);
+      } catch (e) {
+        console.error("Error adding ICE candidate:", e);
+      }
+    }
+  });
+});
